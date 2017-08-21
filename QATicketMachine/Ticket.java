0527f7b83d911423e588daad31ed0f6d0b6cdb88
Farmerman <farmerman.ar@gmail.com>
2017-08-21 17:33:11 +0100

    Created by Coding Ground Client

---
+++ QATicketMachine/Ticket.java
@@ -0,0 +1,14 @@
+public class Ticket
+{
+  public int price;
+  public String type;
+  public Ticket()
+  {
+    price = 0;
+    type = "";
+  }
+  public int getPrice()
+  {
+    return price;
+  }
+}
