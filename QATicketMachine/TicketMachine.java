efbcfc7e9a735574dd60b0e63e3d647a83b90965
Farmerman <farmerman.ar@gmail.com>
2017-08-21 17:37:44 +0100

    Update TicketMachine.java

--- QATicketMachine/TicketMachine.java
+++ QATicketMachine/TicketMachine.java
@@ -1,5 +1,6 @@
 public class TicketMachine
 {
+//Creates a Ticket of the type entered either Student Child Regular or OAP
   public Ticket createTicket(String type)
   {
     Ticket t;
@@ -21,16 +22,20 @@ public class TicketMachine
   private int calcPrice(Ticket[] ticketsToBuy, boolean isWed){
 	   int price = 0;
 	   int discnt = 0;
+	  //If it is a wednesday it will set discnt to 2
 	   if(isWed){
 		   discnt = 2;
 	   }
+	  //Loops through the set of Tickets to be bought
 	   for(Ticket temp : ticketsToBuy){
+		   //price updates with the Tickets price minus the discnt
 		   price += temp.getPrice() - discnt;
        }
 	   return price;
    }
    
    public Ticket[] purchaseTickets(Ticket[] ticketsToBuy, boolean isWed){
+	   //Outputs the amount needed for purchase
 	   System.out.println("The total cost of tickets for this movie is "+calcPrice(ticketsToBuy, isWed)+" GBP.");
 	   return ticketsToBuy;
    }
